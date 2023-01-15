e285be080194415f1d0c466237a78e6dc9c2158c
Paul <paul.ngugi@student.moringaschool.com>
2023-01-16 00:01:09 +0300

    created components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <nav class="navbar bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">The Royal Bank of Flatiron</span>
+  </div>
+</nav>
+    )
+}
+
+export default Header
