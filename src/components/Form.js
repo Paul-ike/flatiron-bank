a4db22bc80b2d13bd03fda7856ac193788f92f67
Paul <paul.ngugi@student.moringaschool.com>
2023-01-16 10:12:16 +0300

    style

--- src/components/Form.js
+++ src/components/Form.js
@@ -32,21 +32,21 @@ function Form({ newData }) {
     return (
         <div className="container">
         <form onSubmit={ handleSubmit }>
-            <div>
-            <div className="py-2">
+            <div className="d-flex justify-content-center py-3">
+            <div className="px-2">
                 <input onChange={ handleChange } type="date" name="date" />
             </div>
-            <div className="py-2">
+            <div className="px-2">
                 <input onChange={ handleChange } type="text" name="description" placeholder="Description" />
             </div>
-            <div className="py-2">
+            <div className="px-2">
                 <input onChange={ handleChange } type="text" name="category" placeholder="Category" />
             </div>
-            <div className="py-2">
+            <div className="px-2">
                 <input onChange={ handleChange } type="number" name="amount" placeholder="Amount" step="0.01" />
             </div>
             </div>
-            <div className="py-2">
+            <div className="text-center py-2">
             <button type="submit" class="btn btn-outline-primary">Add Transaction</button>
             </div>
         </form>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <nav className="navbar">
+        <nav className="navbar bg-primary">
             <div className="container">
             <span className="navbar-brand mb-0 h1">The Royal Bank of Flatiron</span>
             </div>
